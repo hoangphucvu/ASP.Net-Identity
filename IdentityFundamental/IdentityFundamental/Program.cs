14ead57bad3a2d634193df8f700be6c8474ec4b5
ngohungphuc <ngohungphuc95@gmail.com>
2017-05-04 08:12:30 +0700

    adding user using user store

---
+++ IdentityFundamental/IdentityFundamental/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityFundamental
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var creationResult = userManager.Create(new IdentityUser("ngohungphuc95@gmail.com"), "Tony@95");
+            Console.WriteLine("Created: {0}", creationResult.Succeeded);
+        }
+    }
+}
