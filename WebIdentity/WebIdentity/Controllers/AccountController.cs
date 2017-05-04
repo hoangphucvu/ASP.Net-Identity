8e7f4d284f8f68c3d54534f842d104aac95acb65
ngohungphuc <ngohungphuc95@gmail.com>
2017-05-04 15:25:54 +0700

    login & signin

---
+++ WebIdentity/WebIdentity/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace WebIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<IdentityUser> UserManager => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
+        public SignInManager<IdentityUser, string> SignInManager => HttpContext.GetOwinContext().Get<SignInManager<IdentityUser, string>>();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            var signInStatus = await SignInManager.PasswordSignInAsync(model.Username, model.Password, true, true);
+
+            switch (signInStatus)
+            {
+                case SignInStatus.Success:
+                    return RedirectToAction("Index", "Home");
+
+                default:
+                    ModelState.AddModelError("", "Invalid Credentials");
+                    return View(model);
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            var identityUser = await UserManager.FindByNameAsync(model.Username);
+            if (identityUser != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var identityResult = await UserManager.CreateAsync(new IdentityUser(model.Username), model.Password);
+
+            if (identityResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", identityResult.Errors.FirstOrDefault());
+
+            return View(model);
+        }
+    }
+}
