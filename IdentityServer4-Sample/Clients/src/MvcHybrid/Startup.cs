5a4688321acb39df25ff717bad0b81df00acaacd
ngohungphuc <ngohungphuc95@gmail.com>
2017-08-23 21:11:19 +0700

    identity server 4 sample

---
+++ IdentityServer4-Sample/Clients/src/MvcHybrid/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
+using IdentityModel;
+using Clients;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MvcHybrid
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            HostingEnvironment = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (HostingEnvironment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Cookies",
+
+                AutomaticAuthenticate = true,
+
+                ExpireTimeSpan = TimeSpan.FromMinutes(60),
+                CookieName = "mvchybrid"
+            });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                AuthenticationScheme = "oidc",
+                SignInScheme = "Cookies",
+
+                Authority = Constants.Authority,
+                RequireHttpsMetadata = false,
+
+                ClientId = "mvc.hybrid",
+                ClientSecret = "secret",
+
+                ResponseType = "code id_token",
+                Scope = { "openid", "profile", "email", "api1", "offline_access" },
+                GetClaimsFromUserInfoEndpoint = true,
+
+                SaveTokens = true,
+
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = JwtClaimTypes.Name,
+                    RoleClaimType = JwtClaimTypes.Role,
+                }
+            });
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ IdentityServer4-Sample/Clients/src/MvcImplicit/Startup.cs
@@ -0,0 +1,99 @@
+﻿using Clients;
+using IdentityModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace MvcImplicit
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            HostingEnvironment = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (HostingEnvironment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Cookies",
+
+                AutomaticAuthenticate = true,
+
+                ExpireTimeSpan = TimeSpan.FromMinutes(60),
+                CookieName = "mvcimplicit"
+            });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                AuthenticationScheme = "oidc",
+                SignInScheme = "Cookies",
+
+                Authority = Constants.Authority,
+                RequireHttpsMetadata = false,
+
+                ClientId = "mvc.implicit",
+
+                ResponseType = "id_token",
+                Scope = { "openid", "profile", "email" },
+
+                SaveTokens = true,
+
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = JwtClaimTypes.Name,
+                    RoleClaimType = JwtClaimTypes.Role,
+                },
+
+                //Events = new OpenIdConnectEvents
+                //{
+                //    OnRedirectToIdentityProvider = n =>
+                //    {
+                //        // acr_values is where you can pass custom hints/params
+                //        //n.ProtocolMessage.AcrValues = "tenant:foo";
+                //        //n.ProtocolMessage.AcrValues = "idp:Google";
+                //        return Task.FromResult(0);
+                //    }
+                //}
+            });
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
